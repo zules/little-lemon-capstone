bab85522776df3a2bbce62a70b002ab148cfdc9c
zules <5903782+zules@users.noreply.github.com>
2026-04-11 20:00:38 -0500

    finish required unit tests

--- src/BookingPage.test.js
+++ src/BookingPage.test.js
@@ -9,3 +9,14 @@ test('initializeTimes returns the correct initial state', () => {
 
     expect(result).toEqual(placeholderTimes);
 });
+
+test('update times returns the current state', () => {
+
+    const mockState = ["12:00", "13:00"];
+
+    const action = { type: 'updateTimes' };
+
+    const result = timeSlotReducer(mockState, action);
+
+    expect(result).toEqual(mockState);
+});
